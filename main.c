cf5e0ca2c2112a3f06f493a353b1f16862b59754
theuszero <quetzal.noc@gmail.com>
2020-10-19 16:32:58 -0400

    pasar comandos por terminal a programa

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 
 int main(int argc, char **argv) {
     printf("argc: %d\n",argc);
-    printf("argv %s\n",argv[0]);
-    printf("argv %s\n",argv[1]);
-    printf("argv %s\n",argv[2]);
+    if (argc==2){
+        printf("argv %s\n",argv[0]);
+        printf("argv %s\n",argv[1]);
+    } else{
+        return 0;
+    }
     return 0;
 }
