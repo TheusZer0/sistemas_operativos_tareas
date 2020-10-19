83cca8ffb5a4015494b21654dd56f6e386728a4f
theuszero <quetzal.noc@gmail.com>
2020-10-19 16:16:48 -0400

    pasar comandos por terminal a programa

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[1]) {
-    for (int i = 0; i < 2; ++i) {
-        printf("argumento %d, %s \n",i,argv[i]);
+    if (argc==1){
+        printf("this is the command you choose: %s",argv[1]);
     }
     return 0;
 }
