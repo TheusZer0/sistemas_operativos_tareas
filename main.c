6e9bdd200c2f349ee3e4f135cd3b6c03d5566fc5
theuszero <quetzal.noc@gmail.com>
2020-10-31 18:48:22 -0400

    segundo ejercicio

--- main.c
+++ main.c
@@ -1,43 +1,44 @@
-//
-// Created by theuszero on 10/30/20.
-//
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <stdlib.h>
 
 int numArray [50];
-
 float avg_value;
 int min_value;
 int max_value;
 
+pthread_t ID1, ID2, ID3;
+
 void insert_numArray();
-void* return_Avg(void* null);
-void* return_Min(void* null);
-void* return_Max(void* null);
+void* return_avg(void* null);
+void* return_min(void* null);
+void* return_max(void* null);
 
-int main(int argc, char **argv){
+void main(){
 
-    insert_numArray(); //creacion del array
+    insert_numArray();
+    pthread_t tid[3];
+    pthread_attr_t attr[3];
+    pthread_t IDs[3] = {ID1, ID2, ID3};
 
-    pthread_t tid;
-    pthread_attr_t attr;
+    /* se crean los 3 thread*/
+    pthread_attr_init(&attr[0]);
+    pthread_create(&tid[0], &attr[0], return_avg, NULL);
 
-    pthread_attr_init(&attr);
-    pthread_create(&tid, &attr, return_Avg, argv);
-    pthread_join(tid,NULL);
+    pthread_attr_init(&attr[1]);
+    pthread_create(&tid[1], &attr[1], return_min, NULL);
 
-    printf("\n%f\n",avg_value);
+    pthread_attr_init(&attr[2]);
+    pthread_create(&tid[2], &attr[2], return_max, NULL);
+    /*se espera su ejecucion*/
+    pthread_join(tid[0],NULL);
+    pthread_join(tid[1],NULL);
+    pthread_join(tid[2],NULL);
 
 
-    return 0;
+    printf(" Thread ID: %lu calculó promedio y su valor es %f.\n Thread ID: %lu calculó mínimo y su valor es %d.\n Thread ID: %lu calculó máximo y su valor es %d.\n",ID1,avg_value,ID2,min_value,ID3,max_value);
 }
+
 
 void insert_numArray()
 {
@@ -55,31 +56,37 @@ void insert_numArray()
     }
 }
 
-void* return_Avg(void* null){
+void* return_avg(void* null){
+    ID1 = pthread_self();
     float count = 0;
     for (int i = 0; i < 50; ++i) {
         count = count+numArray[i];
     }
     count=count/50;
     avg_value=count;
+    printf("average = %lu\n",pthread_self()); //eliminar esta linea
     return null;
 }
 
-void* return_Min(void* null){
+void* return_min(void* null){
+    ID2 = pthread_self();
     int c, location = 0;
 
     for (c = 1; c < 50; c++)
         if (numArray[c] < numArray[location])
             location = c;
     min_value = numArray[location];
+    printf("minimo = %lu\n",pthread_self()); //eliminar esta linea
     return 0;
 }
 
-void* return_Max(void* null){
+void* return_max(void* null){
+    ID3 = pthread_self();
     int location = 0;
     for (int i = 0; i < 50; ++i) {
         if (numArray[i] > numArray[location])
             location = i;
     }
     max_value = numArray[location];
+    printf("maximo = %lu\n",ID3); // eliminar esta linea
 }
