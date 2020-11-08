e05c84dd8805fa267662b1e03c048401b648868f
jared677 <35714673+jared677@users.noreply.github.com>
2020-11-08 15:34:26 -0300

    Update main.c
    casi push final

--- parte_Dos/main.c
+++ parte_Dos/main.c
@@ -39,20 +39,23 @@ void main(){
     insert_numArray();
     pthread_t tid[3];
     pthread_attr_t attr[3];
-    /* se crean los 3 thread*/
+    
+    /* se inicializan las variables con valores estándar para el thread*/
     pthread_attr_init(&attr[0]);
-    pthread_create(&tid[0], &attr[0], return_avg, NULL);
-
     pthread_attr_init(&attr[1]);
-    pthread_create(&tid[1], &attr[1], return_min, NULL);
-
     pthread_attr_init(&attr[2]);
+    
+    /* se crean los thread*/
+    pthread_create(&tid[0], &attr[0], return_avg, NULL);
+    pthread_create(&tid[1], &attr[1], return_min, NULL);    
     pthread_create(&tid[2], &attr[2], return_max, NULL);
-    /*se espera su ejecucion*/
+    
+    /*se espera a que cada thread termine su ejecución*/
     pthread_join(tid[0],NULL);
     pthread_join(tid[1],NULL);
     pthread_join(tid[2],NULL);
-
+    
+    /* se guardan los ID en un arreglo*/
     pthread_t threadID[3] = {ID1, ID2, ID3};
 
     printf(" Thread ID: %lu calculó promedio y su valor es %f.\n Thread ID: %lu calculó mínimo y su valor es %d.\n Thread ID: %lu calculó máximo y su valor es %d.\n",threadID[0],avg_value,threadID[1],min_value,threadID[2],max_value);
@@ -74,34 +77,40 @@ void insert_numArray()
     }
 }
 
-void* return_avg(){
+void* return_avg(void* ptr_avg_value){
     ID1 = pthread_self();
+    float *avg_value;
+    avg_value = (float *)ptr_avg_value;
     float count = 0;
     for (int i = 0; i < 50; ++i) {
         count = count+numArray[i];
     }
     count=count/50;
-    avg_value=count;
-    return 0;
+    *avg_value=count;
+    return null;
 }
 
-void* return_min(){
+void* return_min(void* ptr_min_value){
     ID2 = pthread_self();
+    int *min_value;
+    min_value = (int *)ptr_min_value;
     int c, location = 0;
 
     for (c = 1; c < 50; c++)
         if (numArray[c] < numArray[location])
             location = c;
-    min_value = numArray[location];
+    *min_value = numArray[location];
     return 0;
 }
 
-void* return_max(){
-    ID3 = pthread_self();
+void* return_max(void* ptr_max_value){
+    ID3 = pthread_self();  
+    int *max_value;
+    max_value = (int *)ptr_max_value;
     int location = 0;
     for (int i = 0; i < 50; ++i) {
         if (numArray[i] > numArray[location])
             location = i;
     }
-    max_value = numArray[location];
+    *max_value = numArray[location];
 }
